393437dacdf1e2c89a917b23ebcc632f06ecf9a9
Yung Dai <yungchidai@gmail.com>
2015-01-29 15:41:12 -0500

    Created the main storyboard and hooked up the elements into view controller.  Wrote code to make the convert button work.

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let sizeFromTextField = mensShoeSizeTextField.text
+        // convert sizeFromTextField into an integer
+        let numberFromTextField = sizeFromTextField.toInt()
+        
+        var integerFromTextField = numberFromTextField!
+        let conversionConstant = 30
+        // remember that this is the same as saying integerFromTextField = integerFromTextField + conversionConstant
+        integerFromTextField += conversionConstant
+        
+        // unhide the label
+        mensConvertedShoeSizeLabel.hidden = false
+        
+        // "\(variable)" is string interpolation
+        let stringWithUpdatedShoeSize = "\(integerFromTextField)"
+        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+    }
 
 }
 
